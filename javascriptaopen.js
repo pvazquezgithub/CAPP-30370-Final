fe670886cad1f6d1d69ad6b3d46de81d3322efbe
pvazquezgithub <philipvazquez@hotmail.com>
2013-12-16 14:08:35 -0500

    aopen
    still need to add the links

---
+++ javascriptaopen.js
@@ -0,0 +1,60 @@
+document.getElementById('wimbledon').addEventListener("click",function(){
+  var input = document.getElementById('wimbledon');    
+  console.log('Alt:'+input.getAttribute('alt'));
+    });
+
+document.getElementById('usopen').addEventListener("click",function(){
+  var input = document.getElementById('usopen');    
+  console.log('Alt:'+input.getAttribute('alt'));
+    });
+document.getElementById('fopen').addEventListener("click",function(){
+  var input = document.getElementById('fopen');    
+  console.log('Alt:'+input.getAttribute('alt'));
+    });
+
+
+document.getElementById('home').addEventListener("click",function(){
+  var input = document.getElementById('home');    
+  console.log('Alt:'+input.getAttribute('alt'));
+    });
+
+
+
+   
+    section2 = $('#section2');
+    
+    $(section2).on('click', '.wimbledon', function(){
+      console.log( 'Width: ' + $(this).width() );
+      console.log( 'Height: ' + $(this).height() );
+    });
+
+
+
+$(section2).on('click', '.usopen', function(){
+    console.log( 'Width: ' + $(this).width() );
+      console.log( 'Height: ' + $(this).height() );
+    });
+$(section2).on('click', '.fopen', function(){
+    console.log( 'Width: ' + $(this).width() );
+      console.log( 'Height: ' + $(this).height() );
+    });
+
+$(section2).on('click', '.home', function(){
+    console.log( 'Width: ' + $(this).width() );
+      console.log( 'Height: ' + $(this).height() );
+    });
+
+$(window).resize(function(e) {
+  console.log( 'Width:' + $(window).width());
+  console.log( 'Height:' + $(window).height());
+    
+});
+
+console.log( 'Width:' + $(window).width());
+  console.log( 'Height:' + $(window).height());
+
+$(header).on('click', function() {
+  console.log('Text:' + $(this).text());
+  console.log('Color:' + $(this).css('color')); 
+  console.log('Font-Size:' + $(this).css('font-size'));
+});
