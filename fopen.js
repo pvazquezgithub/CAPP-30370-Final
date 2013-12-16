50dccebff4e5f95756f66dc2935d4cfc3b58f936
pvazquezgithub <philipvazquez@hotmail.com>
2013-12-16 14:14:56 -0500

    fopen

---
+++ fopen.js
@@ -0,0 +1,61 @@
+document.getElementById('wimbledon').addEventListener("click",function(){
+  var input = document.getElementById('wimbledon');    
+  console.log('Alt:'+input.getAttribute('alt'));
+    });
+
+document.getElementById('usopen').addEventListener("click",function(){
+  var input = document.getElementById('usopen');    
+  console.log('Alt:'+input.getAttribute('alt'));
+    });
+
+document.getElementById('aopen').addEventListener("click",function(){
+  var input = document.getElementById('aopen');    
+  console.log('Alt:'+input.getAttribute('alt'));
+    });
+
+document.getElementById('home').addEventListener("click",function(){
+  var input = document.getElementById('home');    
+  console.log('Alt:'+input.getAttribute('alt'));
+    });
+
+
+
+   
+    section2 = $('#section2');
+    
+    $(section2).on('click', '.wimbledon', function(){
+      console.log( 'Width: ' + $(this).width() );
+      console.log( 'Height: ' + $(this).height() );
+    });
+
+
+
+$(section2).on('click', '.usopen', function(){
+    console.log( 'Width: ' + $(this).width() );
+      console.log( 'Height: ' + $(this).height() );
+    });
+
+$(section2).on('click', '.aopen', function(){
+    console.log( 'Width: ' + $(this).width() );
+      console.log( 'Height: ' + $(this).height() );
+    });
+$(section2).on('click', '.home', function(){
+    console.log( 'Width: ' + $(this).width() );
+      console.log( 'Height: ' + $(this).height() );
+    });
+
+
+$(window).resize(function(e) {
+  console.log( 'Width:' + $(window).width());
+  console.log( 'Height:' + $(window).height());
+    
+});
+
+console.log( 'Width:' + $(window).width());
+  console.log( 'Height:' + $(window).height());
+
+$(header).on('click', function() {
+  console.log('Text:' + $(this).text());
+  console.log('Color:' + $(this).css('color')); 
+  console.log('Font-Size:' + $(this).css('font-size'));
+});
